017c500af72a36e76ba4ed7bf0e283a9b8c5105c
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-05-27 21:56:38 +0000

    displaying of temlates

--- members/views.py
+++ members/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from django.template import loader
+from .models import Members
 
 def greet(request):
-    return render(request,'myfirst.html')
+
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers':mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+  
