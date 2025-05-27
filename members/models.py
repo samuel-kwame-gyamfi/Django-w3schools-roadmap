cdbeca7b88931693b8207cc2de9e141057152ccc
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-05-27 09:29:18 +0000

    created the members model

--- members/models.py
+++ members/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Members(models.Model):
+    first_name = models.CharField(max_length=255)
+    Last_name = models.CharField(max_length=255)
