36da3891095b633fc8cbb726e3d889859934a9ca
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-05-27 09:04:28 +0000

    views,py modified

--- members/views.py
+++ members/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def hello (request):
+def greet (request):
     return HttpResponse("<h1>Hello world</h1>")
