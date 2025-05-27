e40298c058e4850217074dd583ac3652bac87191
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-05-27 19:05:33 +0000

    modfied the Members model

--- members/models.py
+++ members/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Members(models.Model):
     first_name = models.CharField(max_length=255)
     Last_name = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
