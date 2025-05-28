ef4ad7c155f4ada639a70e353abcbe9da8dd6535
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-05-28 05:50:35 +0000

    added an index page

--- members/urls.py
+++ members/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('main/', views.main),
     path('<int:id>', views.details, name='details'),  # Just the ID here
     path('hello/', views.greet),
 ]
