75675752bb2ca701eb4c4d8c299da1c600a0f5f2
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-05-28 05:19:14 +0000

    details.html template

--- members/views.py
+++ members/views.py
@@ -4,11 +4,9 @@ from django.template import loader
 from .models import Members
 
 def greet(request):
-
-    mymembers = Members.objects.all().values()
-    template = loader.get_template('all_members.html')
-    context = {
-        'mymembers':mymembers,
-    }
-    return HttpResponse(template.render(context, request))
-  
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
